4c6fb5e023a5fa7023396b112eb44598161cfb3e
Marnie Reid <marnie@kodewithklossy.com>
2024-04-17 11:57:41 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by scholar on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
